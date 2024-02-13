2514c433f15bbb49dca4b60fa1e6283d13f70766
bravewkc <127928362+bravewkc@users.noreply.github.com>
2024-02-13 14:46:40 +0100

    Add files via upload

---
+++ Form/Wiek.js
@@ -0,0 +1,9 @@
+function showInput(range){
+            rangeValue = range.value;
+            document.querySelector(".showRange").innerHTML = rangeValue;
+            
+}
+function Wyswietl(){
+        let Imie  = document.getElementById("name").innerHTML;
+        console.log(Imie);
+}
